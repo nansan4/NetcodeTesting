6ed6dbbc39b2ea0a36479964d27729fe373aaad6
nansan4 <nando@santosalliances.com>
2025-12-02 17:33:48 -0600

    need to fix player movement

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -1,19 +1,32 @@
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.InputSystem;
 
 public class NetworkPlayer : NetworkBehaviour
 {
-  
+    InputAction moveAction;
+    
+    float moveSpeed = 3f;
+
+    private void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        
+    }
     void Update()
     {
-        Vector3 moveDir = new Vector3(0, 0, 0);
+        if (!IsOwner) return;
 
-        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
-        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
-        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
-        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
 
-        float moveSpeed = 3f;
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        //Vector3 moveDir = new Vector3(0, 0, 0);
+
+        //if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        //if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        //if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        //if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+
+        //float moveSpeed = 3f;
+        //transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
 }
